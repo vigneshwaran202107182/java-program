a949c9c38533f7ab7c65862a08ae3fe98c58fdef
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-11 19:00:17 +0530

    Create is add.java

---
+++ is add.java
@@ -0,0 +1,15 @@
+//Add number
+import java.io.*;
+import java.util*;
+class Addnumber{
+  public static void main(string[] args){
+    Scanner a= new Scanner(System.in);
+    int b=a.nextInt();
+    if(b%2!=0){
+      return 1;
+    }
+    else{
+      return 0;
+    }  
+  }
+}  
