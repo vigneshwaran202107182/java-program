8cce143c6c532e725a78e04f70d4bb3317f6a35d
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-11 19:13:44 +0530

    Update return lastdigit.java

--- return lastdigit.java
+++ return lastdigit.java
@@ -4,4 +4,14 @@ import java.util.*;
 class RturnLastDigit{
   public static void main(stringp[ args){
     Scanner a= new Scanner (System.in);
-    int b=
+    int b=a.nextInt();
+    if (b>0){
+      int c=b%10;
+      return c;
+    }
+    else{
+      int d=b%10*-1;
+      return d;
+    }
+  }  
+}
