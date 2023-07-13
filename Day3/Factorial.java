7380ef06e008f5b59b1fbc14a1a94ed2168eac42
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-13 08:20:21 +0530

    Create Factorial.java

---
+++ Day3/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+imoprt java.i.*;
+
+//Read only region start
+
+class UserMainCode
+{
+public int factorial(int input1)
+{
+//Read only region end
+// Write code here...
+        int res=1;
+		for(int i=1;i<=input1;i++)
+        {
+            res*=i;
+        }
+        return res;
+	}
+}
