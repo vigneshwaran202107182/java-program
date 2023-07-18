beba747adbc5f931d4f9dfe779b19cffb4a9280c
vigneshwaran202107182 <117887936+vigneshwaran202107182@users.noreply.github.com>
2023-07-18 08:26:35 +0530

    Create no of digit.java

---
+++ day4/no of digit.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+// Read only region start
+class UserMainCode
+{
+public int allDigitsCount (int input1){ // Read only region end
+// Write code here...
+int count=0;
+int rem=0;
+while(input1>0)
+{
+rem=input1%10;
+input1/=10;
+count++;
+}
+return count;
+}
+}
